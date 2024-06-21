17ea5037072bab1cac02f79880e6ff6721941275
Jean-Olivier Koulibaly <jeanolivierkoulibaly.dev@gmail.com>
2024-06-21 18:14:37 +0000

    deploy test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,13 @@
-import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req) {
     // console.log('token: ', req.nextauth.token);
 
-    if (!req.nextauth.token?.user.role){
+    if (!req.nextauth.token?.user.role) {
       return NextResponse.redirect(
-        new URL('/api/auth/error?error=AccessDenied', req.url)
+        new URL("/api/auth/error?error=AccessDenied", req.url)
       );
     }
   },
@@ -19,12 +19,5 @@ export default withAuth(
 );
 
 export const config = {
-  matcher: [
-    "/",
-    "/commandes",
-    "/plats",
-    "/menu",
-    "/tables",
-    "/utilisateurs"
-  ],
+  matcher: ["/", "/commandes", "/plats", "/menu", "/tables", "/utilisateurs"],
 };
