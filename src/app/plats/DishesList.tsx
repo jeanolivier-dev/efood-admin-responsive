e733c6935ddaa7720defdf3bce829235e39228d8
ezenardr <ezenardr.dev@gmail.com>
2024-06-25 06:46:10 -0400

    Feat: drawer modify

--- src/app/plats/DishesList.tsx
+++ src/app/plats/DishesList.tsx
@@ -2,18 +2,10 @@
 
 import Image from "next/image";
 import productImage from "@/app/assets/noproduct.jpg";
-import { MoreHorizontal } from "lucide-react";
-
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import {SquarePen, Trash2} from "lucide-react";
+import {Badge} from "@/components/ui/badge";
+import {Button} from "@/components/ui/button";
+import {Card, CardContent, CardFooter} from "@/components/ui/card";
 import {
   Table,
   TableBody,
@@ -22,14 +14,49 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import Link from "next/link";
-import { DisheType } from "@/database/schema";
-import { DeletedDishe } from "@/action/plats";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import {DisheType} from "@/database/schema";
+import {DeletedDishe} from "@/action/plats";
+import {useState} from "react";
 
-export default function DishesList({ dishes }: { dishes: DisheType[] }) {
+type Plate = {
+  name: string;
+  description: string;
+  price: number;
+  is_active: boolean | null;
+}
+
+export default function DishesList({dishes}: { dishes: DisheType[] }) {
+  const [formData, setFormData] = useState<Plate>({
+    description: "",
+    is_active: false,
+    price: 0, name: ""
+  });
+
   async function handleDelete(dishe_id: string) {
     await DeletedDishe(dishe_id);
   }
+
   return (
     <Card>
       <CardContent>
@@ -54,13 +81,13 @@ export default function DishesList({ dishes }: { dishes: DisheType[] }) {
           <TableBody>
             {dishes.map(
               ({
-                dishe_id,
-                name,
-                description,
-                is_active,
-                price,
-                created_at,
-              }) => (
+                 dishe_id,
+                 name,
+                 description,
+                 is_active,
+                 price,
+                 created_at,
+               }) => (
                 <TableRow key={dishe_id}>
                   <TableCell className="hidden sm:table-cell">
                     <Image
@@ -81,31 +108,66 @@ export default function DishesList({ dishes }: { dishes: DisheType[] }) {
                   <TableCell>{price} CFA</TableCell>
                   <TableCell>{created_at && created_at.getUTCDate()}</TableCell>
                   <TableCell>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button
-                          aria-haspopup="true"
-                          size="icon"
-                          variant="ghost"
-                        >
-                          <MoreHorizontal className="h-4 w-4" />
-                          <span className="sr-only">Toggle menu</span>
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="end" className="bg-white">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <Link href="#">
-                          <DropdownMenuItem className="hover:bg-anzac-500 hover:text-white rounded-md">
-                            Modifier
-                          </DropdownMenuItem>
-                        </Link>
-                        <button onClick={() => handleDelete(dishe_id)}>
-                          <DropdownMenuItem className="hover:bg-anzac-500 hover:text-white rounded-md">
-                            Supprimer
-                          </DropdownMenuItem>
-                        </button>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
+                    <div className={"flex gap-4"}>
+                      {/*Edit*/}
+                      <Drawer>
+                        <DrawerTrigger onClick={() => setFormData({
+                          name,
+                          description,
+                          is_active,
+                          price
+                        })}><SquarePen
+                          className={"cursor-pointer"}/></DrawerTrigger>
+                        <DrawerContent>
+                          <DrawerHeader>
+                            <DrawerTitle>Modification de {name}</DrawerTitle>
+                            <DrawerDescription>This action cannot be undone.</DrawerDescription>
+                          </DrawerHeader>
+                          <div>
+                            {/*name*/}
+                            <input type={"text"} value={formData?.name}
+                                   onChange={(e) => setFormData({
+                                     description: formData.description,
+                                     is_active: formData.is_active,
+                                     price: formData.price, name: e.target.value
+                                   })}/>
+                            {/*description*/}
+                            <input type={"text"} value={formData?.description}
+                                   onChange={(e) => setFormData({
+                                     description: e.target.value,
+                                     is_active: formData.is_active,
+                                     price: formData.price, name: formData.name
+                                   })}/>
+                          </div>
+                          <DrawerFooter>
+                            <Button onClick={() => console.log(formData)}>Submit</Button>
+                            <DrawerClose>
+                              <Button variant="outline">Cancel</Button>
+                            </DrawerClose>
+                          </DrawerFooter>
+                        </DrawerContent>
+                      </Drawer>
+                      {/*edit end*/}
+
+                      {/*delete*/}
+                      <AlertDialog>
+                        <AlertDialogTrigger><Trash2 color={"red"} className={"cursor-pointer"}/></AlertDialogTrigger>
+                        <AlertDialogContent>
+                          <AlertDialogHeader>es vous sur de vouloir supprimer ?
+                            <AlertDialogTitle>Et</AlertDialogTitle>
+                            <AlertDialogDescription>
+                              This action cannot be undone. This will permanently delete your account
+                              and remove your data from our servers.
+                            </AlertDialogDescription>
+                          </AlertDialogHeader>
+                          <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction onClick={() => handleDelete(dishe_id)}>Continue</AlertDialogAction>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                      {/*  delete end*/}
+                    </div>
                   </TableCell>
                 </TableRow>
               )
